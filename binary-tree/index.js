bcb971462b396edac619bc1cbae2010f5922b880
Jennifer Bach <jmbach1@gmail.com>
2018-07-07 17:10:25 -0700

    added tests and cleaned up files

--- binary-tree/index.js
+++ binary-tree/index.js
@@ -1,7 +1,6 @@
 'use-strict';
 
-
-class BinaryTree{
+export default class BinaryTree{
   constructor(root = null){
     this.root = root;
   }
@@ -9,7 +8,7 @@ class BinaryTree{
 
   //In-order (left - root - right) traversal
   //Time - O(n) where n is the number of nodes
-  //space - O(n) where n is the height of tree
+  //space - O(h) where n is the height of tree
 
   inOrder() {
     let results = [];
@@ -22,5 +21,36 @@ class BinaryTree{
 
     return results;
   }
+
+  //pre-order (root- left - right) traversal
+  //Time - O(n) where n is the number of nodes
+  //space - O(h) where n is the height of tree
+  preOrder(){
+    let result = [];
+
+    let _walk = (node) => {
+      result.push(node.value);
+      if(node.left) _walk(node.left);
+      if(node.right) _walk(node.right);
+    };
+    _walk(this.root);
+    return result;
+  }
+
+  //post-order (left- right - root) traversal
+  //Time - O(n) where n is the number of nodes
+  //space - O(h) where n is the height of tree
+  postOrder(){
+    let result = [];
+
+    let _walk = (node) => {
+      if(node.left) _walk(node.left);
+      if(node.right) _walk(node.right);
+      result.push(node.value);
+    }
+    _walk(this.root);
+    return result;
+  }
 }
+
 module.exports = BinaryTree;
